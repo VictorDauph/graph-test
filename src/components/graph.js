262e5c7bfd8ee22ea11073307a0575ebf1235d9b
VictorDauph <victor.dauph@gmail.com>
2022-05-07 21:37:04 +0200

    commit init

---
+++ src/components/graph.js
@@ -0,0 +1,64 @@
+// graph to build with reaflow
+// https://openbase.com/categories/js/best-react-graph-libraries
+// https://openbase.com/js/reaflow/tutorials
+import {Canvas} from "reaflow"
+
+const nodes = [
+    {
+      id: '1',
+      text: '1',
+    },
+    {
+      id: '2',
+      text: '2',
+      disabled:true //disabled remove the link
+    },
+    {
+      id: '3',
+      text: '3'
+    },
+    {
+      id: '4',
+      text: '4'
+    }
+  ];
+  
+  const edges = [
+    {
+      id: '1-3',
+      from: '1',
+      to: '3'
+    },
+    {
+      id: '1-2',
+      from: '1',
+      to: '2',
+    },
+    {
+      id: '2-4',
+      from: '2',
+      to: '4'
+    },
+    {
+      id: '2-4',
+      from: '3',
+      to: '4'
+    },
+  ];
+
+
+
+  export default function Graph(){
+    return(
+      <div style={{ position: 'fixed', top: 0, bottom: 0, left: 0, right: 0 }}>
+      <Canvas
+        direction="UP"
+        pannable={false}
+        readonly={true}
+        nodes={nodes}
+        edges={edges}
+      />
+    </div>
+    )
+
+  };
